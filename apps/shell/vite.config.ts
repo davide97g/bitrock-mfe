55a86f66addc1d745c8058e42309363f557863ef
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-05-18 13:07:35 +0200

    test change dir

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-
 const cartHost =
   process.env.NODE_ENV === "prod"
     ? "https://bitrock-mfe-cart.firebaseapp.com"
