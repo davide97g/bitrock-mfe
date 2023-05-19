8c49cb60d800cafeb5e85cf1386b78b6fc84b553
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-05-19 10:27:47 +0200

    comments

--- apps/cart/src/components/Cart.tsx
+++ apps/cart/src/components/Cart.tsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
-import { CartItem } from "types";
+import { CartItem, Item } from "types";
 import { CartItemCard } from "ui";
 
 export default function Cart() {
   const [items, setItems] = useState<CartItem[]>([]);
 
+  // *** EVENT LISTENERS ***
+
   useEffect(() => {
     const handleEventAddToCart = (e: Event) => {
-      const item = (e as CustomEvent<{ item: CartItem }>).detail.item;
+      const item = (e as CustomEvent<{ item: Item }>).detail.item;
       const itemIndex = items.findIndex((i) => i.id === item.id);
       if (itemIndex >= 0) {
         items[itemIndex].quantity += 1;
         setItems([...items]);
-      } else setItems((prev) => [...prev, item]);
+      } else setItems((prev) => [...prev, { ...item, quantity: 1 }]);
     };
+    // ? listen for "cart:add" event (remote event)
     window.addEventListener("cart:add", handleEventAddToCart);
     return () => window.removeEventListener("cart:add", handleEventAddToCart);
   }, [items]);
+
+  // *** INTERNAL FUNCTIONS ***
 
   const removeFromCart = (itemId: string) =>
     setItems(items.filter((item) => item.id !== itemId));
--- apps/shop/src/components/Shop.tsx
+++ apps/shop/src/components/Shop.tsx
@@ -1,5 +1,4 @@
 import { ItemCard } from "ui";
-import { CartItem } from "types";
 import { catalog } from "../assets/catalog";
 
 export default function Shop() {
@@ -7,14 +6,11 @@ export default function Shop() {
     const item = catalog.find((item) => item.id === itemId);
     if (!item) return;
 
-    const cartItem: CartItem = {
-      quantity: 1,
-      ...item,
-    };
+    // *** SEND EVENT ***
     window.dispatchEvent(
       new CustomEvent("cart:add", {
         detail: {
-          item: cartItem,
+          item,
         },
       })
     );
